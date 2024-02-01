e439cab3c8437d4f91866f95f6a2ad43b3fbb301
forneiro <nforneirorosas@gmail.com>
2024-02-01 09:20:57 -0300

    Add Responsive

--- js/index.js
+++ js/index.js
@@ -15,5 +15,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Cambia a la siguiente imagen cada 3 segundos (3000 milisegundos)
   setInterval(nextSlide, 4000);
+
+  const navBar = document.querySelector('.nav-bar');
+  const nav = document.querySelector('.nav');
+
+  navBar.addEventListener('click', () => {
+    if (nav.style.top == '0px') {
+      nav.style.top = '-40rem';
+    } else {
+      nav.style.top = '0';
+    }
+    // nav.classList.toggle('nav-responsive');
+  })
 });
 
